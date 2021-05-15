5b3a4a94197d470bfd3961f02f433ab3620d6068
Mohan Yadav <yadavmohankumarsai@gmail.com>
2021-05-15 17:03:06 +0530

    feat: init custom fonts

--- App.js
+++ App.js
@@ -4,22 +4,68 @@ import { NavigationContainer } from "@react-navigation/native";
 import HomeScreen from "./Home";
 import NewsDetailScreen from "./NewsDetail";
 
+import * as Font from "expo-font";
+import AppLoading from 'expo-app-loading';
+import {
+  Montserrat_100Thin,
+  Montserrat_100Thin_Italic,
+  Montserrat_200ExtraLight,
+  Montserrat_200ExtraLight_Italic,
+  Montserrat_300Light,
+  Montserrat_300Light_Italic,
+  Montserrat_400Regular,
+  Montserrat_400Regular_Italic,
+  Montserrat_500Medium,
+  Montserrat_500Medium_Italic,
+  Montserrat_600SemiBold,
+  Montserrat_600SemiBold_Italic,
+  Montserrat_700Bold,
+  Montserrat_700Bold_Italic,
+  Montserrat_800ExtraBold,
+  Montserrat_800ExtraBold_Italic,
+  Montserrat_900Black,
+  Montserrat_900Black_Italic,
+} from "@expo-google-fonts/montserrat";
+
 const Stack = createStackNavigator();
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      fontsLoaded: false,
+    };
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'MontsLight': Montserrat_300Light,
+      'MontsRegular': Montserrat_400Regular,
+      'MontsMedium': Montserrat_500Medium,
+      'MontsSemibold': Montserrat_600SemiBold,
+      'MontsBold': Montserrat_700Bold,
+      'MontsBoldItalic': Montserrat_700Bold_Italic,
+    });
+
+    this.setState({ fontsLoaded: true });
   }
 
   render() {
-    return (
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="NewsDetail" component={NewsDetailScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
+    const { fontsLoaded } = this.state;
+    console.log(fontsLoaded);
+
+    // Check if font is loaded
+    if (!fontsLoaded) {
+      return (<AppLoading />);
+    } else {
+      return (
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="NewsDetail" component={NewsDetailScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      );
+    }
   }
 }
